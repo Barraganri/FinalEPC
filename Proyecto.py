f17737d9fc007461547542401661c5711feeeb71
Gosflex <114206384+Gosflex@users.noreply.github.com>
2022-09-25 20:11:11 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -3,3 +3,16 @@ archivo = 'C:/Users/Gus/Escuela/5to semestre/EPC/proyecto.xls'#lectura del archi
 
 df = pd.read_excel(archivo, sheet_name='Hoja1')#data frame extraido del excel 
 print(df)#impime el data frame
+#---------Segundo avance
+import pandas as pd
+try:
+    excel = 'C:/Users/Gus/Escuela/5to semestre/EPC/proyecto.xls'
+    res = 0
+    df = pd.read_excel(excel, sheet_name='Hoja1')
+    print(df)
+
+    with open("Archiv01.lst","a") as archivo:
+        while res != 10:
+            print()
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {e} , {type(e)}')
