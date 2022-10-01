d55ac50567513399db3ece101348260741b647b1
Gosflex <114206384+Gosflex@users.noreply.github.com>
2022-10-01 15:25:21 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -1,37 +1,35 @@
-import pandas as pd  # libreria pandas para generar data frames
-
-#---------Segundo avance
-try:
-    excel = '/FinalEPC/proyecto.xls'
-    res = 0
-    df = pd.read_excel(excel, sheet_name='Hoja1')
-    print(df)
-
-    with open("Archiv01.lst","a") as archivo:
-        while res != 10:
-            print()
-except Exception as e:
-    print(f'Exception - Ocurrió un error: {e} , {type(e)}')
-    
-#------------------------Terce avance
-with open("BURBUJA.asc","r") as archivoASC:
+import pandas as pd
+excel = 'proyecto.xls'
+dirEn = ["ORG","EQU","FCB","END"]
+arregl01 = []
+res = 0
+df = pd.read_excel(excel, sheet_name='Hoja1')
+with open("BURBUJA.asc") as archivoASC:
     for i in archivoASC:#lee el archivo y realiza un ciclo
         a=0
-        read = archivoASC.readline()
-        splitt = read.split()
+        splitt = i.split()
+        print(i)
+        #print(splitt)
         #print(splitt)
         for j in splitt:
-            if len(splitt) != 0 and j[0]!="*":
+            if len(splitt) != 0 and j[0]!="*" :
                 #print(j)
                 dfNem=df[df["MNEMONICO"]==str(j.lower())]
                 subset = dfNem[['IMM', 'DIR', 'IND,X','IND,Y', 'EXT', 'INH','REL']]
                 arr = [tuple(x) for x in subset.values]
-                #print(tuples)
+                #print(arr)
                 for y in arr:
                     #print(f"{j} {y} {len(splitt)} {a} {a+1} {splitt}")
-                    if ((a+1) < len(splitt) and splitt[a+1] == y):
-                        print(f"{j} {splitt[a+1]}")
+                    #if ((a+1) < len(splitt) and splitt[a+1] == y):
+                    if ((a+1) < len(splitt)):
+                        #print(f"{j} {splitt[a+1]}")
+                        arregl01.append(splitt[a+1])
+            else:
+                if j[0]=="*" and a>0:
+                    break
+            #print(j)
         a=a+1
+    #print(arregl01)
 
 with open("archiv01.lst","a") as archivo:
     while res != 10:
