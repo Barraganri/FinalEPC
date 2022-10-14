3c1fdea1722386ff0d48cc2c84332ee9648bf152
Gosflex <114206384+Gosflex@users.noreply.github.com>
2022-10-13 19:57:24 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -8,6 +8,7 @@ arregl01 = [] #Provisional para pruebas
 arrS19 = [] #Provisional para pruebas
 var = {} #Diccionario de variables
 const = {} #Dicc de constante
+etique = {}
 res = 0
 df = pd.read_excel(excel, sheet_name='Hoja1') #Dataframe generado a partir del excel para compraraciones
 
@@ -20,11 +21,13 @@ def getOpCode(arreglo):
     return str(arr)
 
 #Compara si dato es contenido en arr
-def compar(dato,arr):
-    res = False
+def comparDire(dato,arr):
+    veri = "a"
     for i in arr:
-        if i == dato:
-            res = True
+        if (i == dato and len(i) == len(dato)):
+            veri = "b"
+        print(f"{i} {j}  {veri} {len(dato)}")
+    print(veri)
     return res
 
 #n = tamano esperado, arr = arreglo a comparar, dato = dato comparado
@@ -59,114 +62,188 @@ def transHex(dato,intt):
             aux = "0" + aux
         elif len(aux) == 3:
             aux = "0" + aux[0] + " " + aux[1:]
-        return aux
+        return aux.upper()
     if(intt == 1):
         aux = dato
         if len(aux) == 1:
             aux = "0" + aux
         elif len(aux) == 3:
             aux = "0" + aux[0] + " " + aux[1:] 
-        return aux
+        return aux.upper()
+    if(intt == 3):
+        aux = str(hex(int(dato)))[2:]
+        if len(aux) == 1:
+            aux = "0" + aux
+        elif len(aux) == 3:
+            aux = "0" + aux[0] + " " + aux[1:]
+        return aux.upper()
 
 #Estructurar el LST
 #linea = linea actual, mnemonico = opcode, oper = operando, lineaCom= linea completa
 def formatoLST(lineas,mnemonico,oper,lineaCom):
     aux = ""
-    if (len(oper) != 0 and  len(mnemonico) != 0 ):
-        aux = str(lineas)+": ("+mnemonico+oper+") " + lineaCom
-        arrS19.append(aux)
+    if (len(str(oper)) != 0 and  len(mnemonico) != 0 ):
+        aux = str(lineas)+" : ("+mnemonico+oper+") :" + lineaCom
+        arrS19.append(aux.upper())
+    elif(len(str(oper)) != 0 and  len(mnemonico) == 0 ):
+        aux = str(lineas)+" : ("+mnemonico+oper+") :" + lineaCom
+        arrS19.append(aux.upper())
+    elif(len(str(oper)) == 0 and  len(mnemonico) != 0 ):
+        aux = str(lineas) +" : ("+mnemonico+oper+") :" + lineaCom
+        arrS19.append(aux.upper())
     else:
-        aux = str(lineas)+": " + lineaCom
-        arrS19.append(aux)
+        aux = str(lineas)+" : VACIO : " + lineaCom
+        arrS19.append(aux.upper())
 
+def etiquetas():
+    auxEtique = {}
+    with open("EJEMPLO.asc") as archivoASC:
+        for i in archivoASC:#lee el archivo y realiza un ciclo
+            a=0 #posicion splitt
+            splitt = str(i.lower()).split()
+            for j in splitt:
+                if len(splitt) != 0 and j[0]!="*" and (a+1) <= len(splitt) and splitt[0]!="*":
+                    dfNem=df[df["MNEMONICO"]==str(j.lower())]
+                    #if para INH
+                    #if (((a+1) == len(splitt) or ((a+1) <= len(splitt) and splitt[a+1][0]=="*" )) and len(dfNem) != 0  ):
+                    if ((a+1) == len(splitt)  and len(dfNem) != 0  ):
+                        if(i[0] == " " ):
+                            break
+                        else:
+                            auxEtique[splitt[0]] = ["G"]
+                            break 
+                    elif((a+1) == len(splitt)  and len(dfNem) == 0 and i[0]!="*" and i[0]!="#" and i[0]!="$"):
+                            auxEtique[splitt[0]] = ["G"]
+                            break
+                else:
+                    a=a+1
+    return auxEtique
+
+def iterador(arreglo, dato):
+    a = 0
+    for x in arreglo:
+        if x == dato:
+            a = 1
+    return a
+            
 
 with open("EJEMPLO.asc") as archivoASC:
-    linea = 0 #contador para lineas
+    linea = 1 #contador para lineas
+    etique=etiquetas()
     for i in archivoASC:#lee el archivo y realiza un ciclo
         a=0 #posicion splitt
         splitt = str(i.lower()).split()
+        if(str(len(splitt)) == "0"):
+            formatoLST(linea,"","","\n")   
         for j in splitt:
+            if(str(len(splitt)) == "0"):
+                formatoLST(linea,"","","")
+                break
             #if ignora comentarios y lineas vacias
-            if len(splitt) != 0 and j[0]!="*" and (a+1) <= len(splitt) :
+            elif len(splitt) != 0 and j[0]!="*" and (a+1) <= len(splitt) and splitt[0]!="*":
                 #Construye dicc var
-                if  len(splitt) > 1 and splitt[1]==operativas[1] and j[1:3]=="00":
-                    var[splitt[0]]=splitt[2]
+                if  len(splitt) > 1 and splitt[1]==operativas[1] and j[0:3]=="$00":
+                    var[splitt[0]]=splitt[2]  
+                    formatoLST(linea,"","",i) 
                     break
                 #Construye dicc const
-                elif  len(splitt) > 1 and splitt[1]==operativas[1] and j[1]=="1":
+                elif  len(splitt) > 1 and splitt[1]==operativas[1] and j[0:2]=="$1":
                     const[splitt[0]]=splitt[2]
-                    break
+                    formatoLST(linea,"","",i)
+                    break 
                 dfNem=df[df["MNEMONICO"]==str(j.lower())]
                 #if para INH
-                if ((a+1) == len(splitt) and len(dfNem) != 0 ):
-                    inh = dfNem[['INH']]
-                    #arregl01.append(j)
-                    arregl01.append(getOpCode(inh))
-                    formatoLST(linea,getOpCode(inh),a,j)
+                #if (((a+1) == len(splitt) or ((a+1) <= len(splitt) and splitt[a+1][0]=="*" )) and len(dfNem) != 0  ):
+                if ((a+1) == len(splitt)  and len(dfNem) != 0  ):
+                    if(i[0] == " " ):
+                        inh = dfNem[['INH']]
+                        #arregl01.append(j)
+                        arregl01.append(getOpCode(inh))
+                        formatoLST(linea,getOpCode(inh),"",i)
+                        break
+                    else:
+                        formatoLST(linea,"","",i)
+                        break 
+                elif((a+1) == len(splitt)  and len(dfNem) == 0 and i[0]!="*" and i[0]!="#" and i[0]!="$"):
+                        formatoLST(linea,"","",i) 
+                        break
+                    #formatoLST(linea,getOpCode(inh),a,j)
                     #print(f"{j} {getOpCode(inh)} ")
                 #ARREGLAR PARA CASO DE COMENTARIOS EN FINAL DE LINEA. HACER FUNCION ISCOMMENT?
                 #if IMM
-                if ((a+1) < len(splitt) and splitt[a+1][0:2] == "#$" and conNum2(Hex,splitt[a+1][2:])):
+                elif ((a+1) < len(splitt) and splitt[a+1][0:2] == "#$" and conNum2(Hex,splitt[a+1][2:])):
                     imm = dfNem[['IMM']]
-                    #arregl01.append(j)
                     arregl01.append(getOpCode(imm))
                     arregl01.append(transHex(splitt[a+1][2:],1)) 
-                    #print(f"{j} {splitt[a+1]}")
+                    formatoLST(linea,getOpCode(imm),transHex(splitt[a+1][2:],1),i)
+                    break
                 #if IMM PERO SIN $
-                if ((a+1) < len(splitt) and splitt[a+1][0] == "#" and conNum2(num,splitt[a+1][1:])):
-                    print(splitt[a+1])
+                elif ((a+1) < len(splitt) and splitt[a+1][0] == "#" and conNum2(num,splitt[a+1][1:])):
                     imm = dfNem[['IMM']]
-                    #arregl01.append(j)
-                    arregl01.append(transHex(splitt[a+1][1:],0))    
-                #    aux = splitt[a+1]
-                #    print(aux)
-                #    #print(f"{j} {splitt[a+1]}")
+                    arregl01.append(transHex(splitt[a+1][1:],0))
+                    formatoLST(linea,getOpCode(imm),transHex(splitt[a+1][1:],0),i)
+                    break
                 #if EXT
-                if((a+1) < len(splitt) and splitt[a+1][0] == "$" and (compar(j,operativas) != True and len(splitt[a+1]) == 5) and conNum2(Hex,splitt[a+1][1:])):
-                    ext = dfNem[['EXT']]
-                #    #arregl01.append(j)
-                    arregl01.append(getOpCode(ext))
-                    arregl01.append(splitt[a+1][1:])
-                #    #print(f"{j} {splitt[a+1]}")
+                elif(((a+1) < len(splitt)) and (splitt[a+1][0] == "$") and (len(splitt[a+1]) == 5) and (conNum(3,operativas,j)== 3) and (conNum2(Hex,splitt[a+1][1:]) == True) ):
+                        ext = dfNem[['EXT']]
+                        arregl01.append(getOpCode(ext))
+                        arregl01.append(splitt[a+1][1:])
+                        formatoLST(linea,getOpCode(ext),splitt[a+1][1:],i)
+                        break
                 #if EXT
-                if((a+1) < len(splitt) and len(splitt[a+1]) == 4 and conNum(4,num,splitt[a+1])  == 4 and (compar(j,operativas) != True)):
+                elif((a+1) < len(splitt) and len(splitt[a+1]) == 4 and conNum(4,num,splitt[a+1])  == 4 ):
                     ext = dfNem[['EXT']]
-                #    #arregl01.append(j)
                     arregl01.append(getOpCode(ext))
                     arregl01.append(transHex(splitt[a+1],0))
-                #    #print(f"{j} {splitt[a+1]}")
-                #if((a+1) < len(splitt) and splitt[a+1][0] == "$" and (compar(j,operativas) != True) and len(splitt[a+1]) == 3 ):
-                    #oper = dfNem[['DIR']]
-                #    #arregl01.append(j)
-                    #arregl01.append(getOpCode(oper))
-                #    arregl01.append(splitt[a+1][1:])
-                #    #print(f"{j} {splitt[a+1]}")
-                #if((a+1) < len(splitt) and len(splitt[a+1]) == 2 and conNum(2,num,splitt[a+1])  == 2 and (compar(j,operativas) != True)):
-                #    oper = dfNem[['DIR']]
-                #    #arregl01.append(j)
-                #    arregl01.append(getOpCode(oper))
-                #    arregl01.append(Hex(splitt[a+1]))
-                #    #print(f"{j} {splitt[a+1]}")
-                ##print(splitt)
-                #print(len(splitt[a+1]))
-                #if((a+1) < len(splitt) and splitt[a+1] == ("#"+str(j))):
-                    #print(f"{j} {splitt[a+1]}")
-                #if((a+1) < len(splitt) and (splitt[a+1] == (str(j)+",X") or splitt[a+1] == (str(j)+",Y"))):
-                    #print(f"{j} {splitt[a+1]}")
-                #if((a+1) < len(splitt) and (splitt[a+1] == (str(j)+",X") or splitt[a+1] == (str(j)+",Y"))):
-                    #print(f"{j} {splitt[a+1]}")
-            else:
-                formatoLST(linea,"","",j)
-                a=a+1
-            #print(j)
-        linea+=1
-    
-    #print(inh)
-    #print(var)
-    print(linea)
-    print(arregl01)
-    print(arrS19)
+                    formatoLST(linea,getOpCode(ext),transHex(splitt[a+1],0),i)
+                    break
+                elif((a+1) < len(splitt) and splitt[a+1][0] == "$" and (conNum2(Hex,splitt[a+1][1:]) == True) and len(splitt[a+1]) == 3 ):
+                    dire = dfNem[['DIR']]
+                    arregl01.append(getOpCode(dire))
+                    arregl01.append(splitt[a+1][1:])
+                    formatoLST(linea,getOpCode(dire),splitt[a+1][1:],i)
+                    break
+                elif((a+1) < len(splitt) and len(splitt[a+1]) == 2 and conNum(2,num,splitt[a+1])  == 2 ):
+                    dire = dfNem[['DIR']]
+                    print(f"{j} {getOpCode(dire)} {transHex(splitt[a+1],3)}")
+                    arregl01.append(getOpCode(dire))
+                    arregl01.append(transHex(splitt[a+1],3))
+                    formatoLST(linea,getOpCode(dire),transHex(splitt[a+1],3),i)
+                    break
+                elif((a+1) < len(splitt) and splitt[a+1][0] == "$" and (conNum2(Hex,splitt[a+1][1:3]) == True) and splitt[a+1][3:5] == ",x" ):
+                    indX = dfNem[['IND,X']]
+                    arregl01.append(getOpCode(indX))
+                    arregl01.append(splitt[a+1][1:3])
+                    formatoLST(linea,getOpCode(indX),splitt[a+1][1:3],i)
+                    break
+                elif((a+1) < len(splitt) and splitt[a+1][0] == "$" and (conNum2(Hex,splitt[a+1][1:3]) == True) and splitt[a+1][3:5] == ",y" ):
+                    if(len(splitt[a+1]) == 5 ):
+                        indY = dfNem[['IND,Y']]
+                        arregl01.append(getOpCode(indY))
+                        arregl01.append(splitt[a+1][1:3])
+                        formatoLST(linea,getOpCode(indY),splitt[a+1][1:3],i)
+                        break
 
+                elif((a+1) < len(splitt) and len(dfNem) != 0 and iterador(etique.keys(),splitt[a+1]) == 1 ):
+                    rel = dfNem[['REL']]
+                    arregl01.append(getOpCode(rel))
+                    formatoLST(linea,getOpCode(rel),"",i)
+                #    break
+            else:
+                if (a==0):
+                    formatoLST(linea,"","",i)
+                a=a+1
+        linea=linea + 1
+    print(arregl01)
+    #print(arrS19)
+
+
+with open("archiv01.lst","a") as archivo:
+    for i in arrS19:
+        archivo.write(i)
+
+#exept Exception as e:
+    #print(f'Exception - Ocurrió un error: {e} , {type(e)}, {}')
 
 with open("archiv01.lst","a") as archivo:
     while res != 10:
